9e40ef2cfb7ce89e984ae97636089c8b488be079
Шайхутдинов Роман Наильевич <rshaykhutdinov@t1-consulting.ru>
2022-10-26 14:05:56 +0300

    1 commit
    2 commit
    
    3 commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,7 @@ public class Main {
     int two = 3;
     int three = 3;
     int four = 4;
-    int five = 5;
+    int five = 1;
     int six = 3;
     int seven = 3;
-    int eight = 4;
-    int nine = 4;
 }
